b5208f62090dc29be714d80b9fb5b4afc8310051
Robin Campbell Joy <rcj@robinjoy.net>
2022-04-22 09:51:43 +0200

    List all tracked files
    Show a view that lists all tracked files for the selected git
    repository and open a selected file.

--- consult-ls-git.el
+++ consult-ls-git.el
@@ -28,16 +28,49 @@
 ;; `consult-ls-git' provides a multi-view for opening files from a git
 ;; repository. It shows modified and untracked files, local branches
 ;; and tags, stashes, open buffers in this repository and a list of
-;; all files.
-
-;; For each view there is also a separate command to show just this
-;; information.
+;; all tracked files.
 
 ;;; Code:
 
 (require 'consult)
 
+(defcustom consult-ls-git-sources
+  '(consult-ls-git--source-tracked-files)
+  "Sources used by `consult-ls-git'")
 
+(defvar consult-ls-git--project-root nil)
+
+(defvar consult-ls-git--source-tracked-files
+  (list :name     "Tracked files"
+        :narrow   '(?f . "Tracked files")
+        :category 'file
+        :face     'consult-file
+        :history  'file-name-history
+        :action   (lambda (f) (consult--file-action (concat consult-ls-git--project-root f)))
+        :items
+        (lambda ()
+          (split-string
+           (shell-command-to-string (format "git -C %s ls-files -z" consult-ls-git--project-root))
+           "\000" 'omit-nulls))))
+
+(defun consult-ls-git--get-project-root ()
+  "Return git project root.
+
+If default-directory isn't inside a git repository, call `project-root' to select a project.
+Returns nil in case no valid project root was found."
+  (or (locate-dominating-file default-directory ".git")
+      (locate-dominating-file (project-prompt-project-dir) ".git")
+      (error "Not a git repository")))
+
+;;;###autoload
+(defun consult-ls-git ()
+  "Create a multi view for current git repository."
+  (interactive)
+  (let ((consult-ls-git--project-root (consult-ls-git--get-project-root)))
+    (consult--multi consult-ls-git-sources
+                    :prompt "Switch to: "
+                    :require-match t
+                    :sort nil)))
 
 (provide 'consult-ls-git)
 ;;; consult-ls-git.el ends here
