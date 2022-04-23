20d6515ba878ed7d588dca59fe068e33f0d7aed9
Robin Campbell Joy <rcj@robinjoy.net>
2022-04-23 15:38:47 +0200

    Update commentary

--- consult-ls-git.el
+++ consult-ls-git.el
@@ -23,12 +23,22 @@
 
 ;;; Commentary:
 
-;; This package provides several consult commands to interact with git.
+;; `consult-ls-git' allows to quickly select a file from a git repository
+;; or act on a stash.  It provides a consult multi view of files
+;; considered by 'git status', stashes as well as all tracked files.
+;; Alternatively you can narrow to a specific section via the shortcut key:
+;;   s: Status
+;;   z: Stash
+;;   f: Tracked Files
 
-;; `consult-ls-git' provides a multi-view for opening files from a git
-;; repository. It shows modified and untracked files, local branches
-;; and tags, stashes, open buffers in this repository and a list of
-;; all tracked files.
+;; If `default-directory' is inside a git repository, it will use this
+;; repository. Otherwise `project-prompt-project-dir' is used to select
+;; the project directory.
+
+;; Each view also has a standalone command in case that is preferable:
+;;   consult-ls-git-status
+;;   consult-ls-git-stash
+;;   consult-ls-git-tracked-files
 
 ;;; Code:
 
